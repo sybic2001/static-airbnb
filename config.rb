93a9b1fcfdfd0e16d73e318b60f0618bccc9c63b
Arthur Vinson <arthur.vinson@polytechnique.org>
2016-05-02 18:32:08 +0200

    Avec amenities

--- config.rb
+++ config.rb
@@ -90,6 +90,6 @@ activate :deploy do |deploy|
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
 
-["johanna", "seb", "romain", "agathe"].each do |name|
+data.flats.keys.each do |name|
   proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner => name }, :ignore => true
 end
